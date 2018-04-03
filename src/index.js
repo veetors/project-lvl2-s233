16d151181a67ffcd7d4fce2d3278a2c9bf8ae0e8
veetors <veetor.s@gmail.com>
2018-04-03 18:40:01 +0300

    refactor diff function

--- src/index.js
+++ src/index.js
@@ -11,36 +11,29 @@ const parsers = {
   '.ini': ini.parse,
 };
 
-export const objToString = (obj) => {
-  const result = Object.keys(obj)
-    .reduce((acc, key) => `${acc}  ${key}: ${obj[key]}\n`, '');
-
-  return `{\n${result}}`;
-};
-
-const getDiffObj = (obj1, obj2) => {
+const comparator = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const commonKeys = [...new Set([...keys1, ...keys2])];
+  const jointKeys = [...new Set([...keys1, ...keys2])];
 
-  const result = commonKeys.reduce((acc, key) => {
+  const result = _.flatten(jointKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
     if (_.has(obj1, key) && _.has(obj2, key)) {
       return value1 === value2 ?
-        { ...acc, [`  ${key}`]: value1 }
+        `    ${key}: ${value1}`
         :
-        { ...acc, [`+ ${key}`]: value2, [`- ${key}`]: value1 };
+        [`  + ${key}: ${value2}`, `  - ${key}: ${value1}`];
     }
 
     return _.has(obj1, key) ?
-      { ...acc, [`- ${key}`]: value1 }
+      `  - ${key}: ${value1}`
       :
-      { ...acc, [`+ ${key}`]: value2 };
-  }, {});
+      `  + ${key}: ${value2}`;
+  })).join('\n');
 
-  return result;
+  return `{\n${result}\n}`;
 };
 
 const genDiff = (path1, path2) => {
@@ -48,8 +41,7 @@ const genDiff = (path1, path2) => {
   const parse = parsers[ext];
   const content1 = parse(fs.readFileSync(path1, 'utf-8'));
   const content2 = parse(fs.readFileSync(path2, 'utf-8'));
-  const diffObj = getDiffObj(content1, content2);
-  const result = objToString(diffObj);
+  const result = comparator(content1, content2);
 
   console.log(result);
   return result;
