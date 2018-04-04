dc02da8613dbb95026d26573ee65ffccdcdc5dde
veetors <veetor.s@gmail.com>
2018-04-04 20:42:33 +0300

    add AST

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,29 +1,56 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('json', () => {
-  const path1 = '__tests__/__fixtures__/json/before.json';
-  const path2 = '__tests__/__fixtures__/json/after.json';
+test('simple json', () => {
+  const path1 = '__tests__/__fixtures__/json/simpleBefore.json';
+  const path2 = '__tests__/__fixtures__/json/simpleAfter.json';
   const result = genDiff(path1, path2);
-  const expected = fs.readFileSync('__tests__/__fixtures__/diff.txt', 'utf-8');
+  const expected = fs.readFileSync('__tests__/__fixtures__/simpleDiff.txt', 'utf-8');
 
   expect(result).toBe(expected);
 });
 
-test('yaml', () => {
-  const path1 = '__tests__/__fixtures__/yaml/before.yml';
-  const path2 = '__tests__/__fixtures__/yaml/after.yml';
+test('simple yaml', () => {
+  const path1 = '__tests__/__fixtures__/yaml/simpleBefore.yml';
+  const path2 = '__tests__/__fixtures__/yaml/simpleAfter.yml';
   const result = genDiff(path1, path2);
-  const expected = fs.readFileSync('__tests__/__fixtures__/diff.txt', 'utf-8');
+  const expected = fs.readFileSync('__tests__/__fixtures__/simpleDiff.txt', 'utf-8');
 
   expect(result).toBe(expected);
 });
 
-test('ini', () => {
-  const path1 = '__tests__/__fixtures__/ini/before.ini';
-  const path2 = '__tests__/__fixtures__/ini/after.ini';
+test('simple ini', () => {
+  const path1 = '__tests__/__fixtures__/ini/simpleBefore.ini';
+  const path2 = '__tests__/__fixtures__/ini/simpleAfter.ini';
   const result = genDiff(path1, path2);
-  const expected = fs.readFileSync('__tests__/__fixtures__/diff.txt', 'utf-8');
+  const expected = fs.readFileSync('__tests__/__fixtures__/simpleDiff.txt', 'utf-8');
+
+  expect(result).toBe(expected);
+});
+
+test('recursive json', () => {
+  const path1 = '__tests__/__fixtures__/json/recursiveBefore.json';
+  const path2 = '__tests__/__fixtures__/json/recursiveAfter.json';
+  const result = genDiff(path1, path2);
+  const expected = fs.readFileSync('__tests__/__fixtures__/recursiveDiff.txt', 'utf-8');
+
+  expect(result).toBe(expected);
+});
+
+test('recursive yaml', () => {
+  const path1 = '__tests__/__fixtures__/yaml/recursiveBefore.yml';
+  const path2 = '__tests__/__fixtures__/yaml/recursiveAfter.yml';
+  const result = genDiff(path1, path2);
+  const expected = fs.readFileSync('__tests__/__fixtures__/recursiveDiff.txt', 'utf-8');
+
+  expect(result).toBe(expected);
+});
+
+test('recursive ini', () => {
+  const path1 = '__tests__/__fixtures__/ini/recursiveBefore.ini';
+  const path2 = '__tests__/__fixtures__/ini/recursiveAfter.ini';
+  const result = genDiff(path1, path2);
+  const expected = fs.readFileSync('__tests__/__fixtures__/recursiveDiff.txt', 'utf-8');
 
   expect(result).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -11,26 +11,83 @@ const parsers = {
   '.ini': ini.parse,
 };
 
-const buildDiffLine = (obj1, obj2) => {
+const getLineFromObject = (obj) => {
+  const keys = _.keys(obj);
+
+  return keys.map(key => `${key}: ${obj[key]}`).join('\n');
+};
+
+const buildDiffTree = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = _.flatten(keys.map((key) => {
+  const tree = _.flatten(keys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      return value1 === value2 ?
-        `    ${key}: ${value1}`
-        :
-        [`  + ${key}: ${value2}`, `  - ${key}: ${value1}`];
+      if (_.isObject(value1) && _.isObject(value2)) {
+        const children = buildDiffTree(value1, value2);
+
+        return { key, type: 'unchanged', children };
+      } else if (_.isObject(value1) || _.isObject(value2)) {
+        return [
+          { key, type: 'removed', value: value1 },
+          { key, type: 'added', value: value2 },
+        ];
+      }
+
+      if (value1 !== value2) {
+        return [
+          { key, type: 'removed', value: value1 },
+          { key, type: 'added', value: value2 },
+        ];
+      }
+
+      return { key, type: 'unchanged', value: value2 };
     }
 
-    return _.has(obj1, key) ?
-      `  - ${key}: ${value1}`
-      :
-      `  + ${key}: ${value2}`;
-  })).join('\n');
+    if (_.has(obj1, key)) {
+      return { key, type: 'removed', value: value1 };
+    }
 
-  return `{\n${result}\n}`;
+    return { key, type: 'added', value: value2 };
+  }));
+
+  return tree;
+};
+
+const buildDiffLine = (diffTree, spaces = 0) => {
+  const result = diffTree.map((node) => {
+    const {
+      type,
+      key,
+      value,
+      children,
+    } = node;
+
+    if (children) {
+      return `${'  '.repeat(spaces)}    ${key}: ${buildDiffLine(children, spaces + 2)}`;
+    }
+
+    if (_.isObject(value)) {
+      if (type === 'removed') {
+        return `${'  '.repeat(spaces)}  - ${key}: ${`{\n${'  '.repeat(spaces + 4)}${getLineFromObject(value)}\n${'  '.repeat(spaces + 2)}}`}`;
+      } else if (type === 'added') {
+        return `${'  '.repeat(spaces)}  + ${key}: ${`{\n${'  '.repeat(spaces + 4)}${getLineFromObject(value)}\n${'  '.repeat(spaces + 2)}}`}`;
+      }
+
+      return `${'  '.repeat(spaces)}    ${key}: ${`{\n${'  '.repeat(spaces + 4)}${getLineFromObject(value)}\n${'  '.repeat(spaces + 2)}}`}`;
+    }
+
+    if (type === 'removed') {
+      return `${'  '.repeat(spaces)}  - ${key}: ${value}`;
+    } else if (type === 'added') {
+      return `${'  '.repeat(spaces)}  + ${key}: ${value}`;
+    }
+
+    return `${'  '.repeat(spaces)}    ${key}: ${value}`;
+  }).join('\n');
+
+  return `{\n${result}\n${'  '.repeat(spaces)}}`;
 };
 
 const genDiff = (path1, path2) => {
@@ -39,7 +96,10 @@ const genDiff = (path1, path2) => {
   const content1 = parse(fs.readFileSync(path1, 'utf-8'));
   const content2 = parse(fs.readFileSync(path2, 'utf-8'));
 
-  return buildDiffLine(content1, content2);
+  const result = buildDiffTree(content1, content2);
+  const resultLine = buildDiffLine(result);
+
+  return resultLine;
 };
 
 export default genDiff;
