ac516afc079890ab2ee2235598c0bc6f8613a2dd
veetors <veetor.s@gmail.com>
2018-04-05 10:59:59 +0300

    add parent property

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,37 +1,36 @@
 import _ from 'lodash';
 
-const buildDiffTree = (obj1, obj2) => {
+const buildDiffTree = (obj1, obj2, parent) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
   const tree = keys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
+    const node = { parent, key };
 
     if (_.has(obj1, key) && _.has(obj2, key)) {
       if (_.isObject(value1) && _.isObject(value2)) {
-        const children = buildDiffTree(value1, value2);
+        const children = buildDiffTree(value1, value2, key);
 
-        return { key, type: 'unchangedParent', children };
+        return { ...node, type: 'unchangedParent', children };
       }
 
       if (value1 !== value2) {
-        const node = {
-          key,
+        return {
+          ...node,
           type: 'changed',
           value: value2,
           previousValue: value1,
         };
-
-        return node;
       }
 
-      return { key, type: 'unchanged', value: value2 };
+      return { ...node, type: 'unchanged', value: value2 };
     }
 
     if (_.has(obj1, key)) {
-      return { key, type: 'removed', value: value1 };
+      return { ...node, type: 'removed', value: value1 };
     }
 
-    return { key, type: 'added', value: value2 };
+    return { ...node, type: 'added', value: value2 };
   });
 
   return tree;
